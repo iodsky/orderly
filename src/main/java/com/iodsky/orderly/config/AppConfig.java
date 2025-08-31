944dd49f49cd9ba432ddf00e09cd979c389656bb
ibrahim <iharby2002@gmail.com>
2025-08-31 23:08:19 +0800

    Feat: Add custom `typeMap` for `ImageDto.product`
    - Configured ModelMapper to map `Product.name` to `ImageDto.product`
    - Ensures DTO exposes product name instead of full Product entity

--- src/main/java/com/iodsky/orderly/config/AppConfig.java
+++ src/main/java/com/iodsky/orderly/config/AppConfig.java
@@ -4,7 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.iodsky.orderly.dto.image.ImageDto;
 import com.iodsky.orderly.dto.product.ProductDto;
+import com.iodsky.orderly.model.Image;
 import com.iodsky.orderly.model.Product;
 
 @Configuration
@@ -16,6 +18,8 @@ public class AppConfig {
 
         mapper.typeMap(Product.class, ProductDto.class).addMapping(src -> src.getCategory().getName(),
                 ProductDto::setCategory);
+
+        mapper.typeMap(Image.class, ImageDto.class).addMapping(src -> src.getProduct().getName(), ImageDto::setProduct);
 
         return mapper;
     }
