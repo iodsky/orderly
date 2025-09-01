5174139a63aaf0df15663911e4d1de70f96f812d
ibrahim <iharby2002@gmail.com>
2025-09-01 15:23:15 +0800

    Feat: add ImageService for managing product images

--- src/main/java/com/iodsky/orderly/service/image/IImageService.java
+++ src/main/java/com/iodsky/orderly/service/image/IImageService.java
@@ -1,4 +1,17 @@
 package com.iodsky.orderly.service.image;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.iodsky.orderly.dto.image.ImageDto;
+
 public interface IImageService {
+  ImageDto getImageById(Long id);
+
+  void deleteImageById(Long id);
+
+  List<ImageDto> saveImage(List<MultipartFile> files, Long productId);
+
+  ImageDto updateImage(MultipartFile file, Long imageId);
 }
