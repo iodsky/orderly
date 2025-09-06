01feea664324bf3b3377953086d149aadc7cc630
ibrahim <iharby2002@gmail.com>
2025-09-06 18:46:36 +0800

    Fix: remove CascadeType.ALL from category association

--- src/main/java/com/iodsky/orderly/model/Product.java
+++ src/main/java/com/iodsky/orderly/model/Product.java
@@ -24,7 +24,7 @@ public class Product {
     private BigDecimal price;
     private int stock;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne()
     @JoinColumn(name = "category_id")
     private Category category;
 
