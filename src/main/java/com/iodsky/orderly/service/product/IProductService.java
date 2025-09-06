2a03ed88bc53b4c031cc103b2d6ebd27cdd22102
ibrahim <iharby2002@gmail.com>
2025-09-06 18:48:42 +0800

    refactor: return entities instead of DTOs
    - Changed `IProductService` method signatures to return `Product` entities instead of `ProductDto`.

--- src/main/java/com/iodsky/orderly/service/product/IProductService.java
+++ src/main/java/com/iodsky/orderly/service/product/IProductService.java
@@ -1,23 +1,22 @@
 package com.iodsky.orderly.service.product;
 
-import com.iodsky.orderly.dto.product.ProductDto;
 import com.iodsky.orderly.dto.product.ProductRequestDto;
 import com.iodsky.orderly.model.Product;
 
 import java.util.List;
 
 public interface IProductService {
-    ProductDto addProduct(ProductRequestDto productRequestDto);
+    Product addProduct(ProductRequestDto productRequestDto);
 
     Product getProductEntity(Long id);
 
-    ProductDto getProductDto(Long id);
+    Product getProductDto(Long id);
 
     void deleteProductById(Long id);
 
-    ProductDto updateProduct(Long productId, ProductRequestDto productRequestDto);
+    Product updateProduct(Long productId, ProductRequestDto productRequestDto);
 
-    List<ProductDto> getProducts(String name, String category, String brand);
+    List<Product> getProducts(String name, String category, String brand);
 
     Long getProductsCountByBrandAndName(String brand, String name);
 
