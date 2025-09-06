993c7958604e751513e621314302e827a6f97d6e
ibrahim <iharby2002@gmail.com>
2025-09-06 18:54:59 +0800

    refactor: add builder support to CategoryDto

--- src/main/java/com/iodsky/orderly/dto/category/CategoryDto.java
+++ src/main/java/com/iodsky/orderly/dto/category/CategoryDto.java
@@ -3,9 +3,11 @@ package com.iodsky.orderly.dto.category;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CategoryDto {
 
   @JsonProperty(access = JsonProperty.Access.READ_ONLY)
