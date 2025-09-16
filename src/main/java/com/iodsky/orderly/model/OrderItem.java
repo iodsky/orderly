9eadcc7a370f2ceba105c1bda4dcd5912593b38f
ibrahim <iharby2002@gmail.com>
2025-09-16 21:22:18 +0800

    Feat: add order item model

---
+++ src/main/java/com/iodsky/orderly/model/OrderItem.java
@@ -0,0 +1,36 @@
+package com.iodsky.orderly.model;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderItem {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @ManyToOne
+  @JoinColumn(name = "order_id")
+  private Order order;
+
+  @ManyToOne
+  @JoinColumn(name = "product_id")
+  private Product product;
+  private int quantity;
+  private BigDecimal price;
+}
