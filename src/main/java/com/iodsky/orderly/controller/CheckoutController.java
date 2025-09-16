295aee50a5c494bdc25e5d9310f5e36ca52b5c25
ibrahim <iharby2002@gmail.com>
2025-09-16 21:27:24 +0800

    feat: add REST controller for checkout

---
+++ src/main/java/com/iodsky/orderly/controller/CheckoutController.java
@@ -0,0 +1,28 @@
+package com.iodsky.orderly.controller;
+
+import com.iodsky.orderly.dto.mapper.OrderMapper;
+import com.iodsky.orderly.dto.order.OrderDto;
+import com.iodsky.orderly.model.Order;
+import com.iodsky.orderly.service.checkout.CheckoutService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/checkout")
+@RequiredArgsConstructor
+public class CheckoutController {
+
+    private final CheckoutService checkoutService;
+    private final OrderMapper orderMapper;
+
+    @PostMapping("{id}")
+    public ResponseEntity<OrderDto> checkoutOrder(@PathVariable UUID id) {
+        Order order = checkoutService.placeOrder(id);
+
+        return new ResponseEntity<>(orderMapper.toDto(order), HttpStatus.CREATED);
+    }
+}
