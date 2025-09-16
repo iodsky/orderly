49e4b330fea687570144f011c366bda99866cee2
ibrahim <iharby2002@gmail.com>
2025-09-16 21:24:38 +0800

    Feat: add OrderMapper

---
+++ src/main/java/com/iodsky/orderly/dto/mapper/OrderMapper.java
@@ -0,0 +1,35 @@
+package com.iodsky.orderly.dto.mapper;
+
+import com.iodsky.orderly.dto.order.OrderDto;
+import com.iodsky.orderly.dto.order.OrderItemDto;
+import com.iodsky.orderly.model.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Component
+public class OrderMapper {
+    public OrderDto toDto(Order order) {
+        if (order == null) {
+            return null;
+        }
+
+        return OrderDto.builder()
+                .id(order.getId())
+                .dateOrdered(order.getDateOrdered())
+                .totalAmount(order.getTotalAmount())
+                .status(order.getOrderStatus())
+                .items(order.getItems()
+                        .stream()
+                        .map(item -> OrderItemDto.builder()
+                                .id(order.getId())
+                                .productId(item.getProduct().getId())
+                                .price(item.getPrice())
+                                .quantity(item.getQuantity())
+                                .productName(item.getProduct().getName())
+                                .build())
+                        .collect(Collectors.toSet()))
+                .build();
+
+    }
+}
