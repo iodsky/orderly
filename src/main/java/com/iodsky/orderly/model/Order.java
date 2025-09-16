707d4e8919832c4916edf003a8e293dc1e8b535a
ibrahim <iharby2002@gmail.com>
2025-09-16 21:22:07 +0800

    Feat: add order model

---
+++ src/main/java/com/iodsky/orderly/model/Order.java
@@ -0,0 +1,40 @@
+package com.iodsky.orderly.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import com.iodsky.orderly.enums.OrderStatus;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "orders")
+public class Order {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private LocalDateTime dateOrdered;
+  private BigDecimal totalAmount;
+
+  @Enumerated(EnumType.STRING)
+  private OrderStatus orderStatus;
+
+  @Builder.Default
+  @EqualsAndHashCode.Exclude
+  @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+  private Set<OrderItem> items = new HashSet<>();
+
+}
