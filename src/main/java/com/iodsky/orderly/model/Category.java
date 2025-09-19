b298414afedb6fdeebccc9538771476a7b91d011
ibrahim <iharby2002@gmail.com>
2025-09-19 15:58:26 +0800

    Refactor: add @Table to Category to enforce consistent SQL naming

--- src/main/java/com/iodsky/orderly/model/Category.java
+++ src/main/java/com/iodsky/orderly/model/Category.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
