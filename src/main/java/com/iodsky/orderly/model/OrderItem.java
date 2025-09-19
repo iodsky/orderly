b02c344171468736adbfd3578e3e9f2da3d555f2
ibrahim <iharby2002@gmail.com>
2025-09-19 15:58:38 +0800

    Refactor: add @Table to OrderItems to enforce consistent SQL naming

--- src/main/java/com/iodsky/orderly/model/OrderItem.java
+++ src/main/java/com/iodsky/orderly/model/OrderItem.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,6 +20,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "order_items")
 public class OrderItem {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
