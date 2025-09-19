1011e877237fb2e6e5c5d15ff4fc728d14b8275f
ibrahim <iharby2002@gmail.com>
2025-09-19 16:00:53 +0800

    Refactor: add @Repository to CartItemRepository to align with Spring best practicesy

--- src/main/java/com/iodsky/orderly/repository/CartItemRepository.java
+++ src/main/java/com/iodsky/orderly/repository/CartItemRepository.java
@@ -3,9 +3,11 @@ package com.iodsky.orderly.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.iodsky.orderly.model.CartItem;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
 
 }
