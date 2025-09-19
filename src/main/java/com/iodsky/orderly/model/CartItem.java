f983cb463002f2a62f7208b1a1f3ec0c722ed72a
ibrahim <iharby2002@gmail.com>
2025-09-19 15:59:07 +0800

    Refactor: add @Table to CartItems to enforce consistent SQL naming

--- src/main/java/com/iodsky/orderly/model/CartItem.java
+++ src/main/java/com/iodsky/orderly/model/CartItem.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,6 +22,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "cart_items")
 public class CartItem {
 
   @Id
