c2882720edfe2a051c9e5b6dfe3b7060d41202a0
ibrahim <iharby2002@gmail.com>
2025-09-21 17:44:26 +0800

    Add Role entity model
    Introduces the Role entity with JPA annotations, including fields for id and role name. This model will be used to represent user roles in the application.

---
+++ src/main/java/com/iodsky/orderly/model/Role.java
@@ -0,0 +1,27 @@
+package com.iodsky.orderly.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String role;
+
+    public Role(String role) {
+        this.role = role;
+    }
+}
