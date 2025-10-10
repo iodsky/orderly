03bedc3b8a27b237f92ac52d45521da0939da4a8
ibrahim <iharby2002@gmail.com>
2025-10-10 21:03:47 +0800

    Chore: update config and SQL initialization for production
    - modified Docker Compose to use /app/application.yml and set SPRING_CONFIG_LOCATION.
    - changed SQL inserts to remove IGNORE for roles and users.
    - updated example.application.yml to remove empty string defaults and set SQL init mode to 'embedded'.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT IGNORE INTO roles (id, role)
+INSERT INTO roles (id, role)
 VALUES (UUID_TO_BIN('1a3f911c-4555-4ba2-8cc4-46d062d4c4c2'), 'ADMIN'),
        (UUID_TO_BIN('d17846b1-bdaf-400b-a0b4-d85fd5e4c453'), 'CUSTOMER');
 
-INSERT IGNORE INTO users (id, email, first_name, last_name, username, password, role_id)
+INSERT INTO users (id, email, first_name, last_name, username, password, role_id)
 VALUES (
         UUID_TO_BIN('8a0b934c-1e51-4079-be33-6e3b40e23b3f'), 'admin@email.com', 'Admin', 'User',
         'admin', '$2a$12$a0vq73QmysMpLEBwBY2AouoeicWYBHOpknxUjYdkgWY9FAogpH4jW',
