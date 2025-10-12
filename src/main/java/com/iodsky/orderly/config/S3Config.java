ee4b88fe1321e6ef0c325b8fd29a6b1b17185fd7
ibrahim <iharby2002@gmail.com>
2025-10-12 12:51:28 +0800

    Feat: use dynamic AWS credentials provider in S3Config
    - S3Config now selects DefaultCredentialsProvider in production and StaticCredentialsProvider with explicit keys in non-production profiles.
    - this improves security by using environment-based credentials in production.

--- src/main/java/com/iodsky/orderly/config/S3Config.java
+++ src/main/java/com/iodsky/orderly/config/S3Config.java
@@ -3,8 +3,7 @@ package com.iodsky.orderly.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.*;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -20,13 +19,24 @@ public class S3Config {
     @Value("${aws.s3.secret-key}")
     private String secretKey;
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     @Bean
     public S3Client s3Client() {
+
+        AwsCredentialsProvider credentialsProvider;
+
+        if (profile.equalsIgnoreCase("production")) {
+            credentialsProvider = DefaultCredentialsProvider.builder().build();
+        } else {
+            credentialsProvider = StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
+        }
+
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
-
 
 }
